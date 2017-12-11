dd141501b39d34e1e3be16c841fc84bf36bd1f75
e30devo <dthomase30@gmail.com>
2017-12-11 15:38:18 -0800

    Added wrong guess bin and Number of Guesses remaining functionaliy

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,12 +12,16 @@ var numberOfGuesses = 10;
 
 // <-- or this... -->
 var wincrementer = 1;
-console.log(wincrementer);
 
 var wincrementerDiv = document.getElementsByClassName("wincrementer")[0];
 
 function runGame () {
 
+function reset () {
+    wordInPlayDiv.textContent = "";
+    numberOfGuesses = 10;
+    runGame();                    
+};  
     // <-- Randomly choose a string from array of subject words -->
     var wordInPlay = subjectWords[Math.floor(Math.random() * subjectWords.length)];
     console.log([...wordInPlay]);
@@ -28,6 +32,7 @@ function runGame () {
 
     var wrongGuessBinDiv = document.getElementsByClassName("wrongGuessBin")[0];
     
+    
     // <-- Display "_ _ _ _ _ _" for wordInPlay -->
         for (var i = 0; i < wordInPlay.length; i++) {
             var span = document.createElement('span');
@@ -36,25 +41,39 @@ function runGame () {
             span.style = 'font-size: 150%; margin: 5px;';
             wordInPlayDiv.appendChild(span);
         };
-        
+        var wrongGuessBin = [];
         document.onkeyup = function(event) {
             var userGuess = String.fromCharCode(event.keyCode).toUpperCase();
+            var numberOfGuessesDiv = document.getElementsByClassName("numberOfGuesses")[0];
+            numberOfGuessesDiv.textContent = "Number of guesses remaining: " + numberOfGuesses;            
             
+            console.log(wrongGuessBin);
             //Log user guess key to appropriate span
             for (var j = 0; j < wordInPlay.length; j++) {
                 if (userGuess === wordInPlay[j]) {
                     letterSpan[j].textContent = userGuess;
-                }                                
+                }
             };
-            
+                if (wordInPlay.indexOf(userGuess) < 0) {
+                    wrongGuessBin.push(userGuess); 
+                    wrongGuessBinDiv.innerHTML = wrongGuessBin;
+                    numberOfGuesses--;
+                    console.log(numberOfGuesses);
+                };
+
                 if (wordInPlayDiv.textContent === wordInPlay) {
-                    wincrementerDiv.textContent = ("Wins " + wincrementer ++);
+                    wincrementerDiv.textContent = ("Wins: " + wincrementer ++ + "!");
+                    wrongGuessBinDiv.textContent = "";
                     console.log(wincrementerDiv.textContent);        
                     document.onkeyup = function(event) {
-                        wordInPlayDiv.textContent = "";
-                        runGame();
-                    };         
-                }               
+                      reset();
+                    };                            
+                };
+
+                if (numberOfGuesses === -1) {
+                    wrongGuessBinDiv.textContent = "";
+                reset();
+                };               
         }; //closer for onkeyup\
     }; //closer for function dec
 runGame();
