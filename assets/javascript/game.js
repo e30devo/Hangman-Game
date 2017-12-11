75855fcac0d4bf442c5ef0af8e81ff24c74fefaa
e30devo <dthomase30@gmail.com>
2017-12-10 23:55:32 -0800

    Cleared out a bunch of commented out operations

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,8 +2,8 @@
   document.addEventListener("DOMContentLoaded", function(event) {
     console.log("DOM fully loaded and parsed");
 
-var subjectWords = [ "BMW", "NISSAN", "CHEVROLET",];
-// "PORSCHE", "VOLKSWAGEN", "HONDA", "FERARRI", "PEUGEOT", "TOYOTA", "AUDI",];
+var subjectWords = [ "BMW", "NISSAN", "CHEVROLET",
+"PORSCHE", "VOLKSWAGEN", "HONDA", "FERARRI", "PEUGEOT", "TOYOTA", "AUDI",];
 console.log(subjectWords);
 
 // <-- if you cant tell what this does... -->
@@ -17,12 +17,10 @@ console.log(wincrementer);
 var wincrementerDiv = document.getElementsByClassName("wincrementer")[0];
 // <-- Randomly choose a string from array of subject words -->
 var wordInPlay = subjectWords[Math.floor(Math.random() * subjectWords.length)];
-// console.log(wordInPlay);
+
 
 var wordInPlayDiv = document.getElementsByClassName("wordInPlay")[0];
-// <-- Convert string to characters to display on key up -->
 
-//RETURN TO ARRAY, COMPARE TO WORD IN PLAY
  
 var letterSpan = document.getElementsByClassName("letters");
 
@@ -39,11 +37,8 @@ var letterSpan = document.getElementsByClassName("letters");
     document.onkeyup = function(event) {
         var userGuess = event.key;
         
-          
-
         //Log user guess key to appropriate span
         for (var j = 0; j < wordInPlay.length; j++) {
-  
             
             if (userGuess === wordInPlay[j]) {
                 letterSpan[j].textContent = userGuess;
@@ -53,54 +48,8 @@ var letterSpan = document.getElementsByClassName("letters");
             };
         } 
         console.log(wordInPlayDiv.textContent);
-        // for (var h = 0; h < wordInPlay.length; h++)
-
-
-
-
-        
-        // if (userGuess !== "N", "I", "S", "A", "N") {
-        //     numberOfGuesses = numberOfGuesses - 1;
-        //     console.log(numberOfGuesses);
-        // }
-            // if (userGuess != letterSpan[i].inne) {
-            //     numberOfGuesses = numberOfGuesses - 1;
-            //     console.log(numberOfGuesses);    
-            // if (numberOfGuesses -1);
-            // }
-        // else (letterSpan[i].textContent === "_") 
-        //     // console.log("BANANAS");
-        
-      
-    //DONT TOUCH FROM HERE UP, THIS ALL WORKS
-        
-        // if (userGuess !== hiddenWord[i]) {
-        //     numberOfGuesses = numberOfGuesses - 1;
-            // console.log(numberOfGuesses);    
-        // if (numberOfGuesses -1);
-        // }
-
-        // if (numberOfGuesses === 0) {
-        //     alert("YOU'VE BEEN PULLED OVER, PAY FINE TO CONTINUE!");
-        // }
-            //     console.log(numberOfGuesses);
-            //     wordInPlay = subjectWords[Math.floor(Math.random() * subjectWords.length)];
-            //     console.log(wordInPlay);
-            // }
-            
-        
-    }
-
-
-    }); //closer for DOM loaded listener
-
-
-// console.log(hiddenWord[0]);
-// (userGuess === hiddenWord.indexOf[i])
-
-// === hiddenWord.indexOf[i]
-
-
+    } //closer for onkeyup\
+}); //closer for DOM loaded listener
 
 // <-- If all letters for word are displayed, show "YOU'VE REACHED TOP SPEED!" to user -->
 
